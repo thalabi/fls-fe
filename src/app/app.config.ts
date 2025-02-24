76930a12392a38df39b8224ccedf4fdf22ef220d
Tarif Halabi <tarif.halabi@gmail.com>
2025-02-23 22:20:44 -0500

    Use a functional http interceptor instead of a DI based one.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,22 @@
-import { ApplicationConfig, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, inject, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
 import MyPreset from './mypreset';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { provideOAuthClient } from 'angular-oauth2-oidc';
 import { provideNgIdle } from '@ng-idle/core';
 import { authAppInitializerFactory } from './auth/auth-app-initializer.factory';
 import { AuthService } from './auth/auth.service';
+import { environment } from '../environments/environment';
+import { httpErrorInterceptor3 } from './http-error-interceptor-3';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
 
         provideAnimationsAsync(),
         providePrimeNG({
@@ -24,16 +27,29 @@ export const appConfig: ApplicationConfig = {
                 }
             }
         }),
+
+        provideHttpClient(
+
+            // DI-based interceptors must be explicitly enabled for HttpErrorInterceptor below to work
+            withInterceptorsFromDi(),
+            withInterceptors([httpErrorInterceptor3]),
+        ),
+
         // for auth module
-        provideHttpClient(),
-        provideOAuthClient(),
-        // for ng idle
-        provideNgIdle(),
-        //
+        provideOAuthClient({
+            resourceServer: {
+                allowedUrls: environment.keycloak.urlPrefixesWithBearerToken,
+                sendAccessToken: true
+            }
+        }),
         provideAppInitializer(() => {
             const authService = inject(AuthService);
             return authAppInitializerFactory(authService)();
-        })
+        }),
 
+
+        // for ng idle
+        provideNgIdle(),
+        //
     ]
 };
