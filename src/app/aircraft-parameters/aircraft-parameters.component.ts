4ac639b690961ff1e4ddd0658185f3cdf4f9c168
Tarif Halabi <tarif.halabi@gmail.com>
2025-02-24 21:57:53 -0500

    wip on aircrap-parameters component.

--- src/app/aircraft-parameters/aircraft-parameters.component.ts
+++ src/app/aircraft-parameters/aircraft-parameters.component.ts
@@ -8,6 +8,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { HalResponseLinks } from '../hal/hal-response-links';
 import { HalResponsePage } from '../hal/hal-response-page';
 import { IGenericEntity } from '../domain/i-gerneric-entity';
+import { MessageService } from 'primeng/api';
 
 @Component({
     selector: 'app-aircraft-parameters',
@@ -33,7 +34,7 @@ export class AircraftParametersComponent implements OnInit {
     loadingStatus!: boolean;
 
 
-    constructor(private restService: RestService) { }
+    constructor(private restService: RestService, private messageService: MessageService) { }
 
     ngOnInit(): void {
         this.form = new FormGroup({
@@ -41,59 +42,69 @@ export class AircraftParametersComponent implements OnInit {
             initialTsmoh: new FormControl<number | null>(null, Validators.required),
             eachTankCapacity: new FormControl<number | null>(null, Validators.required),
             fuelBurnPerHour: new FormControl<number | null>(null, Validators.required),
-        });
-
+        })
         this.restService.getTableData('ac_parameters', '', this.pageNumber, this.ROWS_PER_PAGE, ['registration'])
             .subscribe(
                 {
-                    // next: (flightLogTotalsVResponse: IFlightLogTotalsVResponse) => {
-
-                    //     this.loadingStatus = false
-
-                    //     console.log('flightLogTotalsVResponse', flightLogTotalsVResponse);
-                    //     this.flightLogTotalsVResponse = flightLogTotalsVResponse;
-                    //     this.page = this.flightLogTotalsVResponse.page;
-                    //     this.flightLogTotalsVs = this.page.totalElements ? this.flightLogTotalsVResponse._embedded.flightLogTotalsVs : [];
-                    //     this.clearTimes(this.flightLogTotalsVs);
-                    //     console.log('this.flightLogTotalsVs', this.flightLogTotalsVs);
-                    //     //this.links = this.flightLogTotalsVResponse._links;
-
-                    //     this.calculatePageTotals(this.flightLogTotalsVs)
-                    //     this.pageRowKey = this.flightLogTotalsVs[this.flightLogTotalsVs.length - 1]._links.flightLogTotalsV.href
                     next: rowResponse => {
                         console.log('rowResponse', rowResponse);
                         this.page = rowResponse.page;
                         if (rowResponse._embedded) {
                             this.firstRowOfTable = this.page.number * this.ROWS_PER_PAGE;
-                            //this.rowArray = rowResponse._embedded[GenericEntityService.toPlural(this.entityName)];
                             this.rowArray = rowResponse._embedded.simpleModels;
-                            //ComponentHelper.setRowArrayDateFields(this.rowArray, this.fieldAttributesArray);
                         } else {
                             this.firstRowOfTable = 0;
                             this.rowArray = [];
                         }
                         this.links = rowResponse._links;
-                        this.loadingStatus = false
+
+                        this.initForm()
                     },
                     complete: () => {
-                        console.log('this.flightLogService.getTableData2 completed')
-
-                        // this.messageService.clear()
-                        // this.uploadProgressMessage = '';
-                        // this.uploadResponse = {} as UploadResponse;
-                        // this.messageService.add({ severity: 'info', summary: '200', detail: this.tableFileDownloadProgressMessage })
+                        console.log('this.restService.getTableData completed')
+                        this.loadingStatus = false
                     }
                     ,
                     error: (httpErrorResponse: HttpErrorResponse): void => {
+                        console.log('this.restService.getTableData error')
                         this.loadingStatus = false
-                        // this.messageService.add({ severity: 'error', summary: httpErrorResponse.status.toString(), detail: 'Server error. Please contact support.' })
                     }
                 });
 
     }
 
-    onSubmit() {
+    private initForm() {
+        if (this.rowArray.length > 0) {
+            const acRegistration = this.rowArray[0]
+            this.form.controls['initialTsn'].patchValue(acRegistration['initialTsn'])
+            this.form.controls['initialTsmoh'].patchValue(acRegistration['initialTsmoh'])
+            this.form.controls['eachTankCapacity'].patchValue(acRegistration['eachTankCapacity'])
+            this.form.controls['fuelBurnPerHour'].patchValue(acRegistration['fuelBurnPerHour'])
+        } else {
+            this.form.reset()
+        }
 
+
+    }
+    onSubmit() {
+        let row: IGenericEntity = {} as IGenericEntity
+        row['_links'] = this.rowArray[0]._links
+        row['initialTsn'] = this.form.controls['initialTsn'].value
+        row['initialTsmoh'] = this.form.controls['initialTsmoh'].value
+        row['eachTankCapacity'] = this.form.controls['eachTankCapacity'].value
+        row['fuelBurnPerHour'] = this.form.controls['fuelBurnPerHour'].value
+        this.restService.updateGenericEntity(row).subscribe({
+            next: savedRow => {
+                console.log('updated row', savedRow);
+            },
+            complete: () => {
+                console.log('update row is complete')
+                this.messageService.add({ severity: 'info', summary: '200', detail: 'Updated sucessfully' });
+            },
+            error: error => {
+                console.error('enericEntityService.updateAssociationGenericEntity returned error: ', error);
+            }
+        })
     }
     onCancel() {
 
--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
+import { IGenericEntity } from './domain/i-gerneric-entity';
+import { IGenericEntityResponse } from './response/i-generic-entity-response';
 
 @Injectable({
     providedIn: 'root'
@@ -32,12 +34,42 @@ export class RestService {
         return this.httpClient.get(this.serviceUrl + '/protected/genericEntityController/findAll?' + 'tableName=' + tableName + '&search=' + searchCriteria + '&page=' + pageNumber + '&size=' + pageSize + sortQueryParams + projectionParam)
     }
 
+    updateGenericEntity(row: IGenericEntity): Observable<IGenericEntityResponse> {
+        console.log('row: ', row);
+
+        let url: string = row._links.self.href;
+        console.log('url: ', url);
+        return this.httpClient.patch<IGenericEntity>(url, row).pipe(
+            map((response: any) => {
+                console.log('response', response);
+                return response;
+            }),
+            catchError((httpErrorResponse: HttpErrorResponse) => {
+                console.log('httpErrorResponse', httpErrorResponse);
+                RestService.handleError(httpErrorResponse);
+                return throwError(() => new Error());
+            }));
+    }
+
     public static toCamelCase(tableName: string): string {
         return tableName.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase()); // convert to camel case
     }
 
     public static toPlural(entityName: string): string {
         return entityName.endsWith('s') ? entityName + 'es' : entityName + 's'
+    }
+    public static handleError(httpErrorResponse: HttpErrorResponse) {
+        console.error('An error occurred. See blow info.');
+        console.error('httpErrorResponse', httpErrorResponse);
+        console.error('httpErrorResponse.error', httpErrorResponse.error);
+        console.error('httpErrorResponse.headers', httpErrorResponse.headers);
+        console.error('httpErrorResponse.message', httpErrorResponse.message);
+        console.error('httpErrorResponse.name', httpErrorResponse.name);
+        console.error('httpErrorResponse.ok', httpErrorResponse.ok);
+        console.error('httpErrorResponse.status', httpErrorResponse.status);
+        console.error('httpErrorResponse.statusText', httpErrorResponse.statusText);
+        console.error('httpErrorResponse.type', httpErrorResponse.type);
+        console.error('httpErrorResponse.url', httpErrorResponse.url);
     }
 
 }
